e8a73b12f5083b136ce21b88c08720b55a5ccdde
doanduc2000 <doanminhducvip2000@gmail.com>
2023-09-06 09:20:37 +0700

    first commit

--- database/migrations/2023_08_31_031930_create_userlists_table.php
+++ database/migrations/2023_08_31_031930_create_userlists_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('userlists', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('category_id');
             $table->unsignedBigInteger('user_id');
             $table->string('content');
             $table->timestamps();
